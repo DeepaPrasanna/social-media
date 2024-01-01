485f3b5a029c7fefcd8eadbaaec9d0da43d1d3dd
DeepaPrasanna <anneraj73@gmail.com>
2024-01-01 21:57:10 +0530

    test(backend): :white_check_mark: add tests for auth and user service

--- apps/backend/src/users/users.service.spec.ts
+++ apps/backend/src/users/users.service.spec.ts
@@ -1,18 +1,83 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { UsersService } from './users.service';
+import { UserRepository } from './repositories';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let repository: UserRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: UserRepository,
+          useValue: { findOneByEmail: jest.fn(), create: jest.fn() },
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    repository = module.get<UserRepository>(UserRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findOneByEmail', () => {
+    it('should return a user if found', async () => {
+      const user = {
+        id: 'id',
+        email: 'tom@test.com',
+        firstName: 'Tom',
+        lastName: 'Jerry',
+        password: 'Tom@123',
+        contact: 123456789,
+      };
+
+      jest.spyOn(repository, 'findOneByEmail').mockResolvedValue(user);
+
+      const foundUser = await service.findOneByEmail(user.email);
+
+      expect(foundUser).toEqual(user);
+    });
+
+    it('should return null if user not found', async () => {
+      const user = {
+        id: 'id',
+        email: 'tom@test.com',
+        firstName: 'Tom',
+        lastName: 'Jerry',
+        password: 'Tom@123',
+        contact: 123456789,
+      };
+
+      jest.spyOn(repository, 'findOneByEmail').mockResolvedValue(null);
+
+      const result = await service.findOneByEmail(user.email);
+      expect(result).toBeNull();
+    });
+  });
+
+  describe('create user', () => {
+    it('should create user', async () => {
+      const user = {
+        email: 'tom@test.com',
+        firstName: 'Tom',
+        lastName: 'Jerry',
+        password: 'Tom@123',
+        contact: '123456789',
+      };
+
+      const mockCreateUser = jest
+        .spyOn(repository, 'create')
+        .mockResolvedValue();
+
+      await service.create(user);
+
+      expect(mockCreateUser).toBeCalledWith(user);
+    });
   });
 });
