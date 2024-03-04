232398319c416a1022442064218335f16f57323e
DeepaPrasanna <anneraj73@gmail.com>
2024-03-04 13:52:57 +0530

    refactor(backend): :white_check_mark: pass tests

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -8,6 +8,8 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 
+import { AppController } from './app.controller';
+
 @Module({
   imports: [
     AuthModule,
